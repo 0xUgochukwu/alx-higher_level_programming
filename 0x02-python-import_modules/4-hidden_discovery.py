62b6eb0f7bdfb9948c654344d9ecfe5256485206
0xUgochukwu <0xUgochukwu@gmail.com>
2023-05-11 15:48:37 -0700

    hidden

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3.8
+
+
+if __name__ == "__main__":
+    import hidden_4
+    module_names = dir(hidden_4)
+
+    for name in sorted(module_names):
+        if not name.startswith("__"):
+            print(name)
