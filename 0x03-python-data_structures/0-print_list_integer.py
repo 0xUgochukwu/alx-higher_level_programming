5adb6889cfa35c955f5f9a8f5d0fbff22ac241cf
0xUgochukwu <0xUgochukwu@gmail.com>
2023-05-15 11:22:21 +0000

    print integers

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
-    from add_0 import add
-    a = 1
-    b = 2
-    result = add(a, b)
-    print("{:d} + {:d} = {:d}".format(a, b, result))
+def print_list_integer(my_list=[]):
+	for num in my_list:
+		print("{:d}".format(num))
