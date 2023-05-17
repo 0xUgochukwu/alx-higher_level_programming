3b7307f90045db23ed2419b2039debae2a81baf6
0xUgochukwu <0xUgochukwu@gmail.com>
2023-05-17 16:15:13 -0700

    done

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,11 +1,29 @@
 #include <Python.h>
 #include <stdio.h>
 
+void print_python_list(PyObject *p)
+{
+	PyListObject *list = (PyListObject *)p;
+	Py_ssize_t size = PyList_Size(p);
+	Py_ssize_t i;
+
+	printf("[*] Python list info\n");
+	printf("[*] Size of the Python List = %ld\n", size);
+	printf("[*] Allocated = %ld\n", list->allocated);
+	for (i = 0; i < size; i++)
+	{
+		PyObject *item = PyList_GetItem(p, i);
+		const char *type = item->ob_type->tp_name;
+		printf("Element %ld: %s\n", i, type);
+	}
+}
+
 void print_python_bytes(PyObject *p)
 {
-	long int size;
-	int i;
-	char *trying_str = NULL;
+	PyBytesObject *bytes = (PyBytesObject *)p;
+	Py_ssize_t size = PyBytes_Size(p);
+	char *string = PyBytes_AsString(p);
+	Py_ssize_t i;
 
 	printf("[.] bytes object info\n");
 	if (!PyBytes_Check(p))
@@ -14,35 +32,10 @@ void print_python_bytes(PyObject *p)
 		return;
 	}
 
-	PyBytes_AsStringAndSize(p, &trying_str, &size);
-
-	printf("  size: %li\n", size);
-	printf("  trying string: %s\n", trying_str);
-	if (size < 10)
-		printf("  first %li bytes:", size + 1);
-	else
-		printf("  first 10 bytes:");
-	for (i = 0; i <= size && i < 10; i++)
-		printf(" %02hhx", trying_str[i]);
+	printf("  size: %ld\n", size);
+	printf("  trying string: %s\n", string);
+	printf("  first %ld bytes:", size <= 10 ? size : 10);
+	for (i = 0; i < size && i < 10; i++)
+		printf(" %02hhx", string[i]);
 	printf("\n");
-}
-
-void print_python_list(PyObject *p)
-{
-	long int size = PyList_Size(p);
-	int i;
-	PyObject *item;
-	const char *type;
-
-	printf("[*] Python list info\n");
-	printf("[*] Size of the Python List = %li\n", size);
-	printf("[*] Allocated = %li\n", ((PyListObject *)p)->allocated);
-	for (i = 0; i < size; i++)
-	{
-		item = PyList_GetItem(p, i);
-		type = Py_TYPE(item)->tp_name;
-		printf("Element %i: %s\n", i, type);
-		if (strcmp(type, "bytes") == 0)
-			print_python_bytes(item);
-	}
 }
