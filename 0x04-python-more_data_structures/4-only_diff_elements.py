13706d4771b5fd1275b040c8fc9ba5f43e1b05fe
0xUgochukwu <0xUgochukwu@gmail.com>
2023-05-17 08:36:03 -0700

    not present in both

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 
-def common_elements(set_1, set_2):
-    new_set = set([i for i in set_1 if i not in set_2]
-                  + [i for i in set_2 if i not in set_1])
-
-    return new_set
+def only_diff_elements(set_1, set_2):
+    return list(set_1 - set_2) + list(set_2 - set_1)
