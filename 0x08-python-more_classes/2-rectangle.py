0a7cf3e4ac0f2a3aa5263af7426010895cf24d58
0xUgochukwu <0xUgochukwu@gmail.com>
2023-06-05 05:50:20 -0700

    area & perimeter

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+class Rectangle:
+    """A Rectangle class"""
+
+    def __init__(self, width=0, height=0):
+        """ Initializer of the class """
+
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """ Getter for the width property """
+
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ Setter for width """
+
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+        self.__width = value
+
+    @property
+    def height(self):
+        """ Getter for height property """
+
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ Setter for height """
+
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+
+        self.__height = value
+
+
+    def area(self):
+        """ Calculates the area of the rectangle """
+
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """ Calculates the perimeter of the rectangle """
+
+        return 2(self.__width + self.__height)
