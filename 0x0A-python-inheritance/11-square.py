bcefc55f03aaec789adfd04501741e3d48d6d35c
0xUgochukwu <0xUgochukwu@gmail.com>
2023-06-12 08:43:42 -0700

    square

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -3,24 +3,23 @@
 Square Module
 """
 
-Rectangle = __import__("9-rectangle").Rectangle
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
     """A Square"""
 
     def __init__(self, size):
-        """ Instantiation of the square object """
+        """ Instantion of the square"""
+
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(size, size)
-
-
 
     def area(self):
         """ Area Calculator """
         return self.__size ** 2
 
     def __str__(self):
-        """ Informal string representation of the rectangle """
-        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
+        """ Informal representation of the Square """
+        return "[Square] {:d}/{:d}".format(self.__size, self.__size)
