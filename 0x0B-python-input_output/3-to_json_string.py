35d7f6ad7f96395223edfc452b16bdc83b4e9da2
0xUgochukwu <0xUgochukwu@gmail.com>
2023-06-16 03:48:02 -0700

    to json

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """ Convert to JSON string """
+import json
 
 
-def to_json_string(my_obj):    
+def to_json_string(my_obj):
     """ Convert to JSON string """
     return json.dumps(my_obj)
