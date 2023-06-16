ae6e287d7d66bd758323efaf2e90ab8a000ae6aa
0xUgochukwu <0xUgochukwu@gmail.com>
2023-06-16 03:35:48 -0700

    Wriet

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Write to A File """
+
+
+def write_file(filename="", text=""):
+    """ Function that writes to a file """
+    with open(filename, mode='w', encoding="utf-8") as file:
+        file.write(text)
