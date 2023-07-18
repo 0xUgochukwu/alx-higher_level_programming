2c948895f0704e0870c4304ab625ccac7030b57e
0xUgochukwu <0xUgochukwu@gmail.com>
2023-07-18 10:56:55 -0700

    create bigger table

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,7 +1,7 @@
 -- let's create something bigger
 
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), 
-	score INT);
+score INT);
 
 
 INSERT INTO second_table (id, name, score) VALUES
