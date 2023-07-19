12ddbc3012f2238a733c75d583d5ab63cbb311bc
0xUgochukwu <0xUgochukwu@gmail.com>
2023-07-19 12:20:43 -0700

    privielges

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- let's create a user
 
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEDGES ON *.* TO 'user_0d_1'@'localhost';
-FLUSH PRIVILEDGES;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
