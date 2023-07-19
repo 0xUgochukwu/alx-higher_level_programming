9718cdac77813b18f79fb8910226dee834de0431
0xUgochukwu <0xUgochukwu@gmail.com>
2023-07-19 13:12:57 -0700

    subquery

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- Cities of Carlifonia
 
-SELECT id, name FROM citites WHERE state_id = (SELECT id FROM states WHERE name='California') ORDER BY id ASC;
+SELECT id, name FROM citites WHERE state_id = (SELECT id FROM states WHERE name = "California") ORDER BY id ASC;
