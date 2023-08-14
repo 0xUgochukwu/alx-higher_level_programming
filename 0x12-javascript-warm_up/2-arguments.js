cdf55072df685a262150248fa217b1721092361e
0xUgochukwu <0xUgochukwu@gmail.com>
2023-08-14 23:50:54 +0000

    arguments?

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,9 @@
 #!/usr/bin/node
 
-console.log(process.argv[2] + ' is ' + process.argv[3]);
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
