c7e10e36770919080d97828e3b028c2b89fd0d6c
Ugochukwu Chukwuma <ceeugochukwu@gmail.com>
2023-08-15 23:11:27 +0100

    rotate

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,24 +1,24 @@
 #!/usr/bin/node
 
 module.exports = class Rectangle {
-    constructor (w, h) {
-        if (typeof w === 'number' && typeof h === 'number' && w > 0 && h > 0) {
-            this.width = w;
-            this.height = h;
-        }
+  constructor (w, h) {
+    if (typeof w === 'number' && typeof h === 'number' && w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
+  }
 
-    print () {
-        let h = this.height;
-        while (h-- > 0) {
-            console.log('X'.repeat(this.width));
-        }
+  print () {
+    let h = this.height;
+    while (h-- > 0) {
+      console.log('X'.repeat(this.width));
     }
+  }
 
 
 
-    double () {
-        this.width *= 2;
-        this.height *= 2;
-    }
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
 };
