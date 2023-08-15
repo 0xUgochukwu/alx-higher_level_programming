a4aeaef03867c67bb5274a52678937abee159041
0xUgochukwu <0xUgochukwu@gmail.com>
2023-08-15 15:01:05 -0700

    print

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (typeof w === 'number' && typeof h === 'number' && w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let h = this.height;
+    while (h-- > 0) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+};
