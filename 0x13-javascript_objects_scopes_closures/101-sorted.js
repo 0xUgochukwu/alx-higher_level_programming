4dad2776513e862662faf0cab6a6a0c4a301326f
Ugochukwu Chukwuma <ceeugochukwu@gmail.com>
2023-08-16 00:16:45 +0100

    userOccurence

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const dict = require('./101-data').dict;
+
+const userOccurrences = {};
+
+for (const occurrence in dict.dict) {
+  const userID = dict.dict[occurrence];
+
+  if (!userOccurrences[userID]) {
+    userOccurrences[userID] = [];
+  }
+
+  userOccurrences[userID].push[occurrence];
+};
+
+console.log(userOccurrences);
