1a79f6f5b4f1b18d82ab126dbbe82449975d2cb3
0xUgochukwu <0xUgochukwu@gmail.com>
2023-08-21 03:17:24 -0700

    delete

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -10,14 +10,15 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(argv[1], argv[2], argv[3]),
-                           pool_pre_ping=True)
+            .format(argv[1], argv[2], argv[3]),
+            pool_pre_ping=True)
 
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     db = Session()
 
-    get_matching_states = db.query(State).filter(State.name.like('%a%'))
-    get_matching_states.delete(synchronize_db=False)
+    for instance in session.query(State).filter(State.name.contains('a')):
+            session.delete(instance)
+
     db.commit()
     db.close()
