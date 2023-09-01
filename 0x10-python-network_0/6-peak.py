3cf2f7188c9843fb2ede15d8de797832583f82da
0xUgochukwu <0xUgochukwu@gmail.com>
2023-08-31 18:27:42 -0700

    peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Let's find the peakkkkk!!!!!! """
 
+
 def find_peak(list_of_integers):
     """ finds a peak in a list of unsorted integers  """
     loi = list_of_integers
@@ -10,8 +11,8 @@ def find_peak(list_of_integers):
         return None
 
     m = ln // 2
-    if (m == ln - 1 or loi[m] >= loi[m + 1]) and
-    (m == 0 or loi[m] >= loi[m - 1]):
+    if (m == ln - 1 or loi[m] >= loi[m + 1]) and \
+            (m == 0 or loi[m] >= loi[m - 1]):
         return loi[m]
     if m != ln - 1 and loi[m] < loi[m + 1]:
         return find_peak(loi[:(m + 1)])
